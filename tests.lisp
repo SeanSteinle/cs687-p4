6552859cb1b276cdfa6a2c466c8ae3e3a81e53c7
Sean Steinle <ssteinle@mitre.org>
2024-12-04 10:36:14 -0500

    added sanity tests for existing functions to show how they work

--- tests.lisp
+++ tests.lisp
@@ -1,2 +1,24 @@
 (declaim (sb-ext:muffle-conditions cl:warning))
-(load "utils.lisp")
+(load "p4.lisp")
+
+;UTILITY FUNCTIONS
+
+;;negate
+(let ((pred '(t a-on-table)))
+    (assert (first pred))
+    (assert (not (first (negate pred))))
+)
+
+;;reachable
+(let ((assoc-list '((a . b) (b . c) (c . d) (d . e))))
+  (assert (reachable assoc-list 'a 'e))
+  (assert (not (reachable assoc-list 'e 'a))))
+
+;;cyclic-assoc-list
+(let ((assoc-list1 '((a . b) (b . c) (c . d) (d . a))) (assoc-list2 '((a . b) (b . c) (c . d) (d . e))))
+  (assert (cyclic-assoc-list assoc-list1))
+  (assert (not (cyclic-assoc-list assoc-list2))))
+
+;;binary-combinations
+(assert (= (length (binary-combinations 3)) (expt 2 3)))
+(format t "Binary Combinations for n=3: ~a" (binary-combinations 3))
